df260d941af9a4980eb0290a574c03ec50534019
godwana <godwana07@gmail.com>
2022-03-04 11:10:26 +0100

    Létrehozás

---
+++ src/pkg2d_shapes/Main.java
@@ -0,0 +1,28 @@
+package pkg2d_shapes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Csernay Attila
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
